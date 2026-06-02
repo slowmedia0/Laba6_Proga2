ef5264f2c29be262c16c628b97376a03eff20b9c
slowmedia0 <koselevila50@gmail.com>
2026-06-03 00:15:12 +0300

    Проработанный 5

---
+++ src/common/commands/SaveCommand.java
@@ -0,0 +1,40 @@
+package common.commands;
+
+
+import common.ExitCodeCommand;
+import server.utility.FileManager;
+
+/**
+ * Команда сохранения коллекции в файл.
+ */
+public class SaveCommand extends AbstractCommand {
+    /** Файловый менеджер. */
+    private final FileManager fileManager;
+
+    /**
+     * Создаёт команду {@code save}.
+     *
+     * @param fileManager файловый менеджер
+     */
+    public SaveCommand(FileManager fileManager) {
+        super("save","сохранить коллекцию в файл");
+        this.fileManager = fileManager;
+    }
+
+    public ExitCodeCommand execute(){
+        System.out.println("Сохраняю коллекцию");
+        fileManager.writeCollection();
+        return ExitCodeCommand.OK;
+    }
+
+    @Override
+    public String getArgument() {
+        return "";
+    }
+
+    @Override
+    public ExitCodeCommand validate() {
+        return ExitCodeCommand.OK;
+    }
+
+}
